747d4f88cfaf3f56a942e4372c476c0b75e49566
Ngane Emmanuel <emmanuelngane06@gmail.com>
2024-08-18 05:43:44 -0700

    adding the date picker section for booking modal

--- App/Screen/ServiceDetailScreen/BookingModal.jsx
+++ App/Screen/ServiceDetailScreen/BookingModal.jsx
@@ -1,11 +1,35 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import BackNavigation from '../../Common/BackNavigation'
+import CalendarPicker from "react-native-calendar-picker";
+import Colors from '../../Utils/Colors';
+import Heading from '../../Common/Heading';
 
 export default function BookingModal({hideModal}) {
   return (
     <View style={styles.container}>
-      <BackNavigation handle={false} title={'Book Service'} onclick={() => hideModal()} />
+        {/* back navigation section  */}
+        <BackNavigation 
+        handle={false} 
+        title={'Book Service'} 
+        onclick={() => hideModal()} 
+        />
+        
+        {/* Calendar picker section  */}
+        <View style={{marginTop: 20}}>
+            <Heading text={'Select Date'}/>
+            <View style={styles.calenderContainer} >
+                <CalendarPicker
+                onDateChange={this.onDateChange} 
+                width={340}
+                minDate={Date.now()}
+                todayBackgroundColor={Colors.BLACK}
+                todayTextStyle={{color:Colors.WHITE}}
+                selectedDayColor={Colors.PRIMARY}
+                selectedDayTextColor={Colors.WHITE}
+                />
+            </View>
+        </View>
     </View>
   )
 }
@@ -13,5 +37,12 @@ export default function BookingModal({hideModal}) {
 const styles = StyleSheet.create({
   container: {
     padding: 20
+  },
+
+  calenderContainer: {
+    backgroundColor: Colors.PRIMARY_LIGHT,
+    padding: 20,
+    borderRadius: 15,
+
   }
 })
