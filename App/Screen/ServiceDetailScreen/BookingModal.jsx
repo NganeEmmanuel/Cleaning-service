ababcc470a14dc83bddd3de6d8f192546bd2c4ba
Ngane Emmanuel <emmanuelngane06@gmail.com>
2024-08-18 06:14:02 -0700

    adding the selected time section in booking modal

--- App/Screen/ServiceDetailScreen/BookingModal.jsx
+++ App/Screen/ServiceDetailScreen/BookingModal.jsx
@@ -1,11 +1,43 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import BackNavigation from '../../Common/BackNavigation'
 import CalendarPicker from "react-native-calendar-picker";
 import Colors from '../../Utils/Colors';
 import Heading from '../../Common/Heading';
 
 export default function BookingModal({hideModal}) {
+    const [timeList, setTimeList] = useState([])
+    const [selectedTime, setSelectedTime] = useState()
+    useEffect(() => {
+        getTime()
+    }, [])
+
+    /**
+     * Get time function to generate differnet time intervals from 8Am to 7PM
+     */
+    const getTime = () =>{
+        const timeList =[]
+        for(let i = 8; i <= 12; i++){
+            timeList.push({
+                time: i+':00 AM'
+            })
+            timeList.push({
+                time: i+':30 AM'
+            })
+        }
+    
+        for(let i = 1; i <= 7; i++){
+            timeList.push({
+                time: i+':00 PM'
+            })
+            timeList.push({
+                time: i+':30 PM'
+            })
+        }
+
+        setTimeList(timeList)
+    }
+
   return (
     <View style={styles.container}>
         {/* back navigation section  */}
@@ -30,6 +62,23 @@ export default function BookingModal({hideModal}) {
                 />
             </View>
         </View>
+
+        {/* Select time Section  */}
+        <View style={{marginTop: 20}}>
+            <Heading text={'Select Time'} />
+            <FlatList  
+                data={timeList}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                renderItem={({item, index}) => (
+                    <TouchableOpacity style={{marginRight: 10}} 
+                    onPress={() => setSelectedTime(item.time)}>
+                        <Text style={[selectedTime == item.time?
+                        styles.selectedTime: styles.unselectedTime]}>{item.time}</Text>
+                    </TouchableOpacity>
+                )}
+            />
+        </View>
     </View>
   )
 }
@@ -44,5 +93,24 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 15,
 
+  },
+
+  selectedTime: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor:Colors.PRIMARY,
+    borderRadius: 99,
+    paddingHorizontal: 18,
+    backgroundColor: Colors.PRIMARY,
+    color: Colors.WHITE,
+  },
+
+  unselectedTime: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor:Colors.PRIMARY,
+    borderRadius: 99,
+    paddingHorizontal: 18,
+    color: Colors.PRIMARY,
   }
 })
