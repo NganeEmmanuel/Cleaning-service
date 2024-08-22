faad63b8840ffcca8ccb0d6f3bf9e12137a316f7
Ngane Emmanuel <emmanuelngane06@gmail.com>
2024-08-22 02:21:35 -0700

    adding the load service implementation in the myserviceScreen

--- App/Screen/MyServiceScreen/MyServiceScreen.jsx
+++ App/Screen/MyServiceScreen/MyServiceScreen.jsx
@@ -1,9 +1,26 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import ServiceActions from './ServiceActions'
 import Heading from './../../Common/Heading'
+import { useUser } from '@clerk/clerk-expo'
+import GlobalApi from './../../Utils/GlobalApi'
+import ActiveService from './ActiveService'
 
 export default function MyServiceScreen() {
+
+    const [services, setServices] = useState()
+    const {user} = useUser()
+
+    useEffect(() => {
+        getServiceByUserEmail()
+    }, [user, services])
+
+    const getServiceByUserEmail = () => {
+        GlobalApi.getServicesByUserEmail(user?.primaryEmailAddress.emailAddress)
+        .then(resp => {
+            setServices(resp?.serviceLists)
+        })
+    }
   return (
     <ScrollView>
         {/* heading section  */}
@@ -17,7 +34,14 @@ export default function MyServiceScreen() {
         {/* active service section  */}
         <View style={{padding: 20, paddingBottom: 0}}>
             <Heading text={'Active Services'} />
-        
+            <View>
+                <FlatList 
+                data={services}
+                renderItem={({item, index}) => (
+                    <ActiveService  />
+                )}
+                />
+            </View>
         </View>
     </ScrollView>
   )
